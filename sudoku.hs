bc4f9b4e243c233295ca54ca58aca2bb583a7957
Goran Butorac <goran.butorac@bankrate.com>
2019-10-16 17:28:41 -0400

    Add brute-force solver
    ! Never finishes because there is 6670903752021072936960 (6.7×10^21) variations

--- sudoku.hs
+++ sudoku.hs
@@ -3,14 +3,31 @@ module Main where
 import Data.List
 import Control.Monad.Random
 
-main :: IO ()
-main = random9x9 >>= prettyPrint
-
 random9 :: IO [Int]
 random9 = liftM (take 9 . nub) $ getRandomRs (1, 9)
 
-random9x9 :: IO [[Int]]
-random9x9 = sequence (replicate 9 random9)
+random81 :: IO [[Int]]
+random81 = sequence (replicate 9 random9)
 
 prettyPrint :: [[Int]] -> IO ()
 prettyPrint s = sequence (map print s) >> return ()
+
+main :: IO ()
+main = do
+  s <- random81
+  if conforms s then
+    prettyPrint s
+  else do
+    -- print $ nub $ map (\x -> (s !! x) !! 0) [0..8]
+    -- prettyPrint s
+    -- putChar '.'
+    main
+
+conforms :: [[Int]] -> Bool
+conforms s = map nub ff == ff
+  where
+    f c = map (\x -> (s !! x) !! c) [0..8]
+    ff = map f [0..8]
+
+
+
