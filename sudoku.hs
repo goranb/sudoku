805f2d51e1625aa01c96600d39d5a89965a97e7d
Goran Butorac <goran.butorac@bankrate.com>
2019-10-16 16:53:32 -0400

    Add prettyPrint

--- sudoku.hs
+++ sudoku.hs
@@ -4,10 +4,13 @@ import Data.List
 import Control.Monad.Random
 
 main :: IO ()
-main = random9x9 >>= print
+main = random9x9 >>= prettyPrint
 
 random9 :: IO [Int]
 random9 = liftM (take 9 . nub) $ getRandomRs (1, 9)
 
 random9x9 :: IO [[Int]]
 random9x9 = sequence (replicate 9 random9)
+
+prettyPrint :: [[Int]] -> IO ()
+prettyPrint s = sequence (map print s) >> return ()
