aa2255f4fd39c12a355f75d39b5a37fbfab79c1c
Goran Butorac <goran.butorac@bankrate.com>
2019-10-16 10:51:27 -0400

    Initial commit
    - compiles, but replicates the same random sequence for all 9 rows
    - stated a "pretty printer" that should show the 9 blocks in sudoku format

---
+++ sudoku.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import System.Random
+import Data.List
+import Control.Monad
+
+main :: IO ()
+main = do
+  r99 <- random99
+  putStr . show $ r99
+
+randomDigits :: IO [Int]
+randomDigits = do
+  gen <- getStdGen
+  return $ randomRs (1,9) gen
+
+random9 :: IO [Int]
+random9 = do
+  digits <- randomDigits
+  return $ take 9 $ nub digits
+
+random99 :: IO [[Int]]
+random99 = replicate9 9 random9
+
+replicate9 :: Int -> IO [Int] -> IO [[Int]]
+replicate9 0 _ = return []
+replicate9 x y = do
+  r9 <- random9
+  (:) r9 <$> replicate9 (x - 1) y
+
+-- prettyPrint :: IO [[Int]] -> IO ()
+-- prettyPrint p = do
+--   map (\l -> fmap (\d -> print d) l) p
