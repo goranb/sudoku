c4291bc895a9f5e0738902d923c81dd529c04f77
Goran Butorac <git@gbut.com>
2019-10-19 23:45:02 -0400

    Working with rows and columns

--- sudoku.hs
+++ sudoku.hs
@@ -14,20 +14,56 @@ prettyPrint s = sequence (map print s) >> return ()
 
 main :: IO ()
 main = do
-  s <- random81
-  if conforms s then
-    prettyPrint s
-  else do
-    -- print $ nub $ map (\x -> (s !! x) !! 0) [0..8]
-    -- prettyPrint s
-    -- putChar '.'
-    main
+  r81 <- random81
+  prettyPrint $ conform r81 []
 
-conforms :: [[Int]] -> Bool
-conforms s = map nub ff == ff
+conform :: [[Int]] -> [[Int]] -> [[Int]]
+conform []     o = o
+conform (r:rs) o = conform rs $ o ++ [sortRow r o]
+
+column :: Int -> [[Int]] -> [Int]
+column _ [] = []
+column c o  = map (\r -> r !! c) o
+
+sortRow :: [Int] -> [[Int]] -> [Int]
+sortRow r [] = map (\i -> sortCell i r [] []) [0..8]
+sortRow r o  = map (\(i, c) -> sortCell i r c o) columns
   where
-    f c = map (\x -> (s !! x) !! c) [0..8]
-    ff = map f [0..8]
+  columns = map (\n -> (n, column n o)) [0..8]
+
+sortCell :: Int -> [Int] -> [Int] -> [[Int]] -> Int
+sortCell i r c  _  = r !! i * 1000000 + div (sum c) 1000000
 
 
 
+
+
+  -- print $ place r9 [[],[],[],[],[],[],[],[],[]]
+
+
+  -- s <- random81
+  -- if conforms s then
+  --   prettyPrint s
+  -- else do
+  --   main
+
+
+-- conforms :: [[Int]] -> Bool
+-- conforms s = map nub ff == ff
+--   where
+--     f c = map (\x -> (s !! x) !! c) [0..8]
+--     ff = map f [0..8]
+
+
+-- place :: Int -> [[Int]] -> [[Int]]
+-- place num grid = newGrid where
+--   index = findIndex (\row -> length row < 9) grid
+
+-- getRow :: Maybe Int -> [[Int]] -> [Int]
+-- getRow (Just num) grid = grid !! num
+
+-- placeInRow :: Maybe Int -> [Int] -> [Int]
+-- placeInRow (Just num) row =
+--   if num `elem` row then row
+--   else row ++ [num]
+-- placeInRow Nothing row = row
