793db5d55ef4574252d3012d9aea29aafe54e5b8
Goran Butorac <git@gbut.com>
2019-10-20 01:50:37 -0400

    Brute force. Let remove the indexing now

--- sudoku.hs
+++ sudoku.hs
@@ -1,45 +1,79 @@
 module Main where
 
-import Data.List
+-- import Data.List
+
 import Control.Monad.Random
 
-
-
 random9 :: IO [Int]
-random9 = liftM (take 9 . nub) $ getRandomRs (1, 9)
+random9 = getRandomRs (1, 9)
 
-random81 :: IO [[Int]]
-random81 = sequence (replicate 9 random9)
+-- random9 :: IO [Int]
+-- random9 = liftM (take 9 . nub) $ getRandomRs (1, 9)
+
+-- random81 :: IO [[Int]]
+-- random81 = sequence (replicate 9 random9)
 
 prettyPrint :: [[Int]] -> IO ()
 prettyPrint s = sequence (map print s) >> return ()
 
 main :: IO ()
 main = do
-  r81 <- random81
-  prettyPrint $ conform $ index r81
+  r9 <- random9
+  let con = conform 0 0 r9 []
+  prettyPrint $ group 9 $ con
+  -- print $ column 8 con
+  -- print $ row 8 con
+  -- print $ quadrant 2 2 con
 
 
-conform :: [[(Int, Int)]] -> [[Int]]
-conform o = [ [ q | c <- r,
-                    let n = snd c,
-                    let i = fst c,
-                    let q = n * 1000 + i,
-                    let x = i `mod` 9,
-                    let y = i `div` 9,
-                    let row = map (\c -> snd c) $ o !! y,
-                    let col = column x o
+conform :: Int -> Int -> [Int] -> [Int] -> [Int]
+conform _     81 _      o = o
+conform 1000000 _  _      o = o
+conform z     i  (s:ss) o = if s `elem` r || s `elem` c || s `elem` q then conform (z+1) ii ss ooo
+                      else conform (z+1) (i+1) ss oo
+                      where
+                        x = i `mod` 9
+                        y = i `div` 9
+                        r = row y o
+                        c = column x o
+                        q = quadrant (x `div` 3) (y `div` 3) o -- it's not a `quadrant` :P
+                        oo = o ++ [s]
+                        ooo = if z `mod` 1000 == 0 then [] else o
+                        ii = if z `mod` 1000 == 0 then 0 else i
 
-                    ] | r <- o ]
-                    -- i <- r
-                    -- ]]
-column :: Int -> [[(Int, Int)]] -> [Int]
+
+                     -- if (s `elem` (row (i)))
+
+group :: Int -> [a] -> [[a]]
+group _ [] = []
+group n l
+  | n > 0 = (take n l) : (group n (drop n l))
+  | otherwise = error "Negative or zero n"
+
+row :: Int -> [a] -> [a]
+row _ [] = []
+row r o  = take 9 $ drop (r * 9) o
+
+column :: Int -> [a] -> [a]
 column _ [] = []
-column c o  = map (\r -> snd $ r !! c) o
+column c o = every 9 $ drop c o
+
+every :: Int -> [a] -> [a]
+every n [] = []
+every n as  = head as : every n (drop n as)
+
+quadrant :: Int -> Int -> [a] -> [a]
+quadrant x y o = q1 ++ q2 ++ q3
+  where
+  q1 = take 3 $ drop (x*3) $ row (y*3) o
+  q2 = take 3 $ drop (x*3) $ row (y*3 + 1) o
+  q3 = take 3 $ drop (x*3) $ row (y*3 + 2) o
+
+-- every n = everyf n . drop (n-1)
 
 
-index :: [[Int]] -> [[(Int, Int)]]
-index o =  map (\r -> map (\i -> (i, o !! (i `div` 9) !! (i `mod` 8))) [(0 + r*9 :: Int)..(8 + r*9 :: Int)]) [0..8]
+-- index :: [[Int]] -> [[(Int, Int)]]
+-- index o =  map (\r -> map (\i -> (i, o !! (i `div` 9) !! (i `mod` 8))) [(0 + r*9 :: Int)..(8 + r*9 :: Int)]) [0..8]
 
 -- conform :: [[Int]] -> [[Int]] -> [[Int]]
 -- conform []     o = o
