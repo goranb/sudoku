a00f2bde915697069b61d0901723c1af363418bd
Goran Butorac <git@gbut.com>
2019-10-20 00:19:40 -0400

    List comprehension?

--- sudoku.hs
+++ sudoku.hs
@@ -3,6 +3,8 @@ module Main where
 import Data.List
 import Control.Monad.Random
 
+
+
 random9 :: IO [Int]
 random9 = liftM (take 9 . nub) $ getRandomRs (1, 9)
 
@@ -15,24 +17,46 @@ prettyPrint s = sequence (map print s) >> return ()
 main :: IO ()
 main = do
   r81 <- random81
-  prettyPrint $ conform r81 []
+  prettyPrint $ conform $ index r81
 
-conform :: [[Int]] -> [[Int]] -> [[Int]]
-conform []     o = o
-conform (r:rs) o = conform rs $ o ++ [sortRow r o]
 
-column :: Int -> [[Int]] -> [Int]
+conform :: [[(Int, Int)]] -> [[Int]]
+conform o = [ [ q | c <- r,
+                    let n = snd c,
+                    let i = fst c,
+                    let q = n * 1000 + i,
+                    let x = i `mod` 9,
+                    let y = i `div` 9,
+                    let row = map (\c -> snd c) $ o !! y,
+                    let col = column x o
+
+                    ] | r <- o ]
+                    -- i <- r
+                    -- ]]
+column :: Int -> [[(Int, Int)]] -> [Int]
 column _ [] = []
-column c o  = map (\r -> r !! c) o
+column c o  = map (\r -> snd $ r !! c) o
 
-sortRow :: [Int] -> [[Int]] -> [Int]
-sortRow r [] = map (\i -> sortCell i r [] []) [0..8]
-sortRow r o  = map (\(i, c) -> sortCell i r c o) columns
-  where
-  columns = map (\n -> (n, column n o)) [0..8]
 
-sortCell :: Int -> [Int] -> [Int] -> [[Int]] -> Int
-sortCell i r c  _  = r !! i * 1000000 + div (sum c) 1000000
+index :: [[Int]] -> [[(Int, Int)]]
+index o =  map (\r -> map (\i -> (i, o !! (i `div` 9) !! (i `mod` 8))) [(0 + r*9 :: Int)..(8 + r*9 :: Int)]) [0..8]
+
+-- conform :: [[Int]] -> [[Int]] -> [[Int]]
+-- conform []     o = o
+-- conform (r:rs) o = conform rs $ o ++ [sortRow r o]
+
+-- column :: Int -> [[Int]] -> [Int]
+-- column _ [] = []
+-- column c o  = map (\r -> r !! c) o
+
+-- sortRow :: [Int] -> [[Int]] -> [Int]
+-- sortRow r [] = map (\i -> sortCell i r [] []) [0..8]
+-- sortRow r o  = map (\(i, c) -> sortCell i r c o) columns
+--   where
+--   columns = map (\n -> (n, column n o)) [0..8]
+
+-- sortCell :: Int -> [Int] -> [Int] -> [[Int]] -> Int
+-- sortCell i r c  _  = r !! i * 1000000 + div (sum c) 1000000
 
 
 
